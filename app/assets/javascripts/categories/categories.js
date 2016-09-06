de19c68628e0ca87b18858bc5fda83ec194719b4
Mara Sanders <sandersmara@gmail.com>
2016-09-06 09:25:24 -0400

    heroku deployment

--- app/assets/javascripts/categories/categories.js
+++ app/assets/javascripts/categories/categories.js
@@ -3,52 +3,33 @@
 (function(){
   angular
   .module("categories", ["ngResource"])
-  .controller("CategoryIndexController", [
-    "$resource",
-    "CategoryFactory",
-    "$stateParams",
-    "$http",
-    "$state",
-    CategoryIndexController
-  ])
+  .controller("CategoryController", ["$resource", CategoryController]);
 
-  //Index all the Categories
-    function CategoryIndexController($resource, CategoryFactory, $stateParams, $http, $state){
-      var vm = this;
-      vm.category_data = {}
-  //Get categories from the database
-      var Category = $resource("/categories/:id.json", {}, {
-        update: {method: "PUT"}
+  function CategoryController($resource){
+    var vm = this;
+    var Category = $resource("/categories/:id.json", {}, {
+      update: {method: "PUT"}
+    });
+    //  console.log(CategoryFactory)
+    // vm.category_data = CategoryFactory.query();
+
+    vm.sort_category_data_by = function(name){
+      vm.sort_on = name;
+      vm.is_descending = !(vm.is_descending);
+    }
+
+
+    vm.destroy = function(category_index){
+      var category = vm.category_data[category_index];
+      Category.remove(category_index, function(response){
+        if(response.success) vm.category_data.splice(category_index, 1);
       });
-      vm.category_data = CategoryFactory.query();
+    }
 
-  //Delete a Category
-      vm.destroy = function(cat){
-        Category.remove({id: cat.id}, function(response){
-          if(response.success) vm.category_data.splice(vm.category_data.indexOf(cat), 1);
-        });
-      }
-
-  //Create a Category throw an error if name is blank
-      vm.new_category = new CategoryFactory();
-      vm.create = function(){
-        if (vm.new_category.Cat_name){
-          Category.save(vm.new_category, function(response){
-            if(response.success) vm.catgory_data.push(response);
-              vm.new_catgory = new CategoryFactory();
-            })
-            vm.category_data.push(angular.copy(vm.new_category));
-        }else{
-            alert("Category can not be blank!!");
-        }
-        vm.new_category = {};
-      }
-
-  //Edit Category
-      vm.update = function(category){
-        Category.update({id: category.id}, category, function(response){
-          category.showEdit = !category.showEdit
-        });
-      }
-    } //Close CategoryIndexController
-  })();
+    vm.update = function(category){
+      Category.update({id: category.id}, category, function(response){
+        console.log("Category updated!");
+      });
+    }
+  }
+})();
